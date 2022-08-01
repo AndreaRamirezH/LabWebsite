814186898faf2d1fc72059752f97bdea039e16c2
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-08-01 18:13:19 +0000

    Deploying to gh-pages from  @ 35a089f2a4ad7dbb328e9c4ea39981c4c8288485 🚀

--- assets/people/code/teamcards.jl
+++ assets/people/code/teamcards.jl
@@ -1,8 +1,9 @@
 # This file was generated, do not modify it. # hide
 #hideall
 past_team = [
-  (name="Adriano Bellotti", pic="", title="Ph.D. Student", vitae="currently somewhere else", email=""),
-  (name="Adrianna R Loback", pic="", title="", vitae="currently...", email=""),
+  (name="Adriano Bellotti", pic="", title="Ph.D. Student", vitae="currently somewhere else", email="new email"),
+  (name="Adrianna R Loback", pic="", title="Research Associate", vitae="currently...", email="new email"),
+  (name="Saeed Aljaberi", pic="", title="Ph.D. Student", vitae="", email="new email")
   ]
 
 "@@cards @@row" |> println
