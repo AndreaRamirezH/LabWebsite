089843ce836cc53b91e5e9c24f8c88723a5cebdb
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2023-01-18 16:23:08 +0000

    Deploying to gh-pages from  @ 40808a0e220505bd231419500ffbf2385080f263 🚀

--- assets/people/code/teamcards.jl
+++ assets/people/code/teamcards.jl
@@ -4,7 +4,6 @@ past_team = [
   (name="Saeed Aljaberi", pic="avatar.jpg", title="Ph.D. Student", vitae="", email=""),
   (name="Adriano Bellotti", pic="avatar.jpg", title="Ph.D. Student", vitae="", email=""),
   (name="Kristine Heiney", pic="Kristine.jpg", title="Ph.D. Student", vitae="", email="kh732@cam.ac.uk"),
-  (name="Dr. Monika Jozsa", pic="avatar.jpg", title="Research Associate", vitae="", email="mj555@cam.ac.uk"),
   (name="Adrianna R Loback", pic="avatar.jpg", title="Research Associate", vitae="", email=""),
   (name="Dr. Dhruva Raman", pic="avatar.jpg", title="Research Associate", vitae="",  email="dvr23@cam.ac.uk")
   ]
